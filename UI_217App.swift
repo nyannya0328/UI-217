98709c55b31dbbe03b58604bafc5d7244b3f701b
nyannya0328 <thecuba54@gmail.com>
2021-06-02 22:38:58 +0900

    UI-217

---
+++ UI_217App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_217App.swift
+//  UI-217
+//
+//  Created by にゃんにゃん丸 on 2021/06/02.
+//
+
+import SwiftUI
+
+@main
+struct UI_217App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
